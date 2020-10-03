9323ed3b21f3423b01f341f2e0615bbc840355d5
Leonardo Jose de Lima <leonardoblizzard@hotmail.com>
2020-10-03 10:38:51 -0300

    Adicionado cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { db } from './databaseConnect.js';
 
@@ -24,6 +25,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
