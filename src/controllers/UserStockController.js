233ba0092dfc2152d435bc0d25d7a022ef886852
Leonardo <leonardoblizzard@hotmail.com>
2021-04-24 14:53:52 -0300

    Criado APIs para controle de stocks do usuário

--- src/controllers/UserStockController.js
+++ src/controllers/UserStockController.js
@@ -5,6 +5,7 @@ import MathValidations from '../utils/MathValidations.js';
 const expenses = db.expenses;
 const Accounts = db.accounts;
 const goals = db.goals;
+const clientStocks = db.clientStocks;
 
 class UserStockController{
     async AddUserStockGoal(req, res, next){
@@ -13,9 +14,9 @@ class UserStockController{
 
             const stockTotalValue = stockPrice * stockQtd
 
-           await UpdateGoal(req, next, stockTotalValue);
+            await UpdateGoal(req, next, stockTotalValue);
                 
-           return res.json("Valor " + `${ stockTotalValue } ` + " retirado da meta teste para o investimento TESTE, efetuado com sucesso !");
+            return res.json("Valor " + `${ stockTotalValue } ` + " retirado da meta teste para o investimento TESTE, efetuado com sucesso !");
         
         }catch(error){
             next(error);
@@ -36,6 +37,47 @@ class UserStockController{
             await UpdateAccount(req, accountExist, stockTotalValue, next);
 
             return res.json("Valor " + `${ stockTotalValue } ` + " retirado da conta corrente para o investimento TESTE, efetuado com sucesso !");
+
+        }catch(error){
+            next(error);
+        }
+    }
+
+    async GetUserStocks(req, res, next){
+        try{
+
+            const userId = req.params.userId
+
+            const existStock = await clientStocks.aggregate([
+                { $match : 
+                    {
+                         parentClientId : userId
+                    }
+                },
+                {
+                    $lookup:{
+                        from: "StockMarketPlace",     
+                        localField: "parentStockIdObject",  
+                        foreignField: "_id", 
+                        as: "user_stock" 
+                    }
+                }
+            ]);
+            
+            if(!existStock)
+                throw new Error("Não encontrado nenhum investimento na base de dados !");
+
+            const data =[];
+
+            existStock.forEach(x => {
+                data.push({
+                    id: x.parentStockId,
+                    stockDisplayName: x.user_stock[0].stockDisplayName,
+                    totalInvestment: x.totalValue
+                })
+            })
+
+            return res.json(data);
 
         }catch(error){
             next(error);
@@ -81,13 +123,23 @@ async function UpdateAccount(req, account, stockTotalValue, next){
         }
 
         if(AccountUpdate)
+        {
             await AddExpense(req, stockTotalValue, newAccount, next);  
+            await AddClientStocks(next, stockTotalValue, req, account._id)
+        }
+           
 }
 
 async function UpdateGoal(req, next, stockTotalValue){
 
-    try{
-        var goal = await GoalsController.GetGoal(req, next);
+        const { parentId } = req.body;
+
+        const goal = await GoalsController.GetGoal(req, next);
+
+        var userAccount = await Accounts.findOne({ _id : parentId });
+
+        if(userAccount.id !== goal.parentId)
+            throw new Error('Nenhuma meta chamada ' + `${goal.nameGoal } ` + ', foi encontrada para o usuário' + `${userAccount.name }`);
     
         if(goal.currentGoalValue < stockTotalValue)
             throw new Error('O valor contido na meta ' + `${goal.nameGoal } ` + 'não é sufuciente para efetuar a transação !');
@@ -99,11 +151,24 @@ async function UpdateGoal(req, next, stockTotalValue){
         });
 
         if(goalUpdate)
-            await AddExpense(req, stockTotalValue, goal, next);      
+        {
+            await AddExpense(req, stockTotalValue, goal, next);
+            await AddClientStocks(next, stockTotalValue, req, goal.parentId);
+        }
+}
 
-    }catch(error){
-        next(error);
-    }
+async function AddClientStocks(next, stockTotalValue, req, idCLiente){
+    
+    const{ stockQtd, stockId } = req.body;
+
+        await clientStocks.create({
+            parentClientId: idCLiente,
+            parentStockIdObject: stockId,
+            parentStockId: stockId,
+            NumberOfStocks: stockQtd,
+            totalValue: stockTotalValue,
+        });
+
 }
 
 export default new UserStockController();
---
+++ src/models/ClientStocks.js
@@ -0,0 +1,31 @@
+
+export default(mongoose) => {
+    const Schema = mongoose.Schema
+    const schema = mongoose.Schema({
+
+        parentClientId:{
+            type: String,
+            required: true
+        },
+        parentStockIdObject:{
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        parentStockId:{
+            type: String,
+            required: true
+        },
+        NumberOfStocks:{
+            type: String,
+            required: true
+        },
+        totalValue:{
+            type: Number,
+            required: true
+        }
+    })
+
+    const clientStocksSchema = mongoose.model('ClientStocks', schema, 'ClientStocks');
+
+    return clientStocksSchema;
+}
