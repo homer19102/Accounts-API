1957e6be9cc4f3df36ef950383f9b4ed0be6f5a5
Leonardo <leonardoblizzard@hotmail.com>
2021-08-21 19:57:21 -0300

    Ajuste JWT

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export default (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if(req.originalUrl === '/login/' || req.originalUrl === /NewUser/)
+    if(req.originalUrl === '/login/' || req.originalUrl === '/accounts/NewUser/')
         return next();
 
     if(!authHeader)
